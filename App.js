38544ec8ce95c18b2125f55a695a58e3e5263909
p00gz <bloopsocket@gmail.com>
2020-05-21 18:46:02 -0500

    update

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
@@ -7,13 +9,13 @@ const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
-    List: ListScreen
+    List: ListScreen,
   },
   {
     initialRouteName: 'List',
     defaultNavigationOptions: {
-      title: 'App'
-    }
+      title: 'App',
+    },
   }
 );
 
