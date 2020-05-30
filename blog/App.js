edafb59283d106c0081e5a94886da94608ea590a
p00gz <bloopsocket@gmail.com>
2020-05-29 19:24:04 -0500

    update

--- blog/App.js
+++ blog/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator(
   {
-    Index: IndexScreen
+    Index: IndexScreen,
   },
   {
     initialRouteName: 'Index',
     defaultNavigationOptions: {
-      title: 'Blogs'
-    }
+      title: 'Blogs',
+    },
   }
 );
 
-const App = createAppContainer(navigator);
-
-export default () => {
-  return <App />;
-};
+export default createAppContainer(navigator);
