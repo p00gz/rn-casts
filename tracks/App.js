f4cdce1f5e578edcef0b8bca4bd85c69336433e4
p00gz <bloopsocket@gmail.com>
2020-06-22 19:08:24 -0500

    update

--- tracks/App.js
+++ tracks/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {
-  createAppContainer,
-  createStackNavigator,
-  createBottomTabNavigator,
-  createSwitchNavigator
-} from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -17,16 +15,16 @@ import { setNavigator } from './src/navigationRef';
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
-    Signin: SigninScreen
+    Signin: SigninScreen,
   }),
   mainFlow: createBottomTabNavigator({
     trackListFlow: createStackNavigator({
       TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen
+      TrackDetail: TrackDetailScreen,
     }),
     TrackCreate: TrackCreateScreen,
-    Account: AccountScreen
-  })
+    Account: AccountScreen,
+  }),
 });
 
 const App = createAppContainer(switchNavigator);
@@ -35,7 +33,7 @@ export default () => {
   return (
     <AuthProvider>
       <App
-        ref={navigator => {
+        ref={(navigator) => {
           setNavigator(navigator);
         }}
       />
