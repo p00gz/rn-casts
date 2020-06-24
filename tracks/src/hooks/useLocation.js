30e12f67d65c46c1655a0233c84ee6b76635173e
p00gz <bloopsocket@gmail.com>
2020-06-23 19:31:38 -0500

    update

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -2,20 +2,23 @@ import { useState, useEffect } from 'react';
 import {
   Accuracy,
   requestPermissionsAsync,
-  watchPositionAsync
+  watchPositionAsync,
 } from 'expo-location';
 
-export default callback => {
+export default (callback) => {
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
     try {
-      await requestPermissionsAsync();
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
       const subscriber = await watchPositionAsync(
         {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
-          distanceInterval: 10
+          distanceInterval: 10,
         },
         callback
       );
